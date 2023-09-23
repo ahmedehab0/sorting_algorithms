954580cab99aa40d173d8ee90a0c43bad4b446ea
ahmedehab0 <ahmed.eh01@gmail.com>
2023-09-22 20:43:40 -0400

    fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -60,12 +60,16 @@ int partition(int *array, int low, int high, size_t size)
 	{
 		if (array[j] < pivot)
 		{
-			swap(&array[j], &array[i]);
-			print_array(array, size);
-			i++;
+			if (i < j)
+			{
+				swap(&array[j], &array[i]);
+				print_array(array, size);
+				i++;
+			}
 		}
 	}
 	swap(&array[i], &array[high]);
+	print_array(array, size);
 	return (i);
 }
 
