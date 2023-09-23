3781130bc4e250bedcf1e03c4f305e3dc4b8f097
ahmedehab0 <ahmed.eh01@gmail.com>
2023-09-22 20:26:56 -0400

    3

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+/**
+ *swap - swaps two elements
+ *
+ *@xp: pointer to the first element
+ *@yp: pointer to the second element
+ */
+void swap(int *xp, int *yp)
+{
+int temp;
+
+temp = *xp;
+*xp = *yp;
+*yp = temp;
+}
+
+/**
+ *quick_sort - function that sorts an array using quick sort alogorithm
+ *@array: array of integers
+ *@size: number of elements in the array
+ *
+ */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_recursion(array, 0, (int) size - 1, size);
+}
+/**
+ *quick_sort_recursion - function that sorts an array using quick sort alogorithm
+ *
+ *@array: array of integers
+ *@low: first index
+ *@high: last index
+ */
+void quick_sort_recursion(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high, size);
+		quick_sort_recursion(array, low, pivot_index - 1, size);
+		quick_sort_recursion(array, pivot_index + 1, high, size);
+	}
+}
+/**
+ *partition - function that sorts an array using quick sort alogorithm
+ *@array: array of integers
+ *@low: first index
+ *@high: last index
+ *
+ * Return: pivot
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot, i, j;
+
+	pivot = array[high];
+	i = low;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swap(&array[j], &array[i]);
+			print_array(array, size);
+			i++;
+		}
+	}
+	swap(&array[i], &array[high]);
+	return (i);
+}
+
--- sort.h
+++ sort.h
@@ -8,6 +8,9 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void swap(int *xp, int *yp);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursion(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
